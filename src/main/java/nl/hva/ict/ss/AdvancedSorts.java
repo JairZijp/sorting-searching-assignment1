8e35ba4aae2c4ff81e5e835f6515aff1a000805c
JairZijp <jairzijp123@gmail.com>
2018-11-27 21:55:09 +0100

    quicksort normal array

--- src/main/java/nl/hva/ict/ss/AdvancedSorts.java
+++ src/main/java/nl/hva/ict/ss/AdvancedSorts.java
@@ -1,6 +1,8 @@
 package nl.hva.ict.ss;
 
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class AdvancedSorts {
 
@@ -20,8 +22,119 @@ public class AdvancedSorts {
      * @param <E>
      * @return
      */
-    public static <E extends Comparable<E>> E[] quickSort(E[] unsorted) {
-        return unsorted;
+    public static <E extends Comparable<E>> E[] quickSort(E[] unsorted) { 
+        shuffle(unsorted);
+        E[] sortedArray;
+        sortedArray = (E[]) sort(unsorted, 0, unsorted.length - 1);
+      
+        return sortedArray;
+    }
+    
+    // quicksort the subarray from a[lo] to a[hi]
+    private static Comparable[] sort(Comparable[] a, int lo, int hi) { 
+        if (hi <= lo) return a;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+        return a;
     }
 
+    // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+    // and return the index j.
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) { 
+
+            // find item on lo to swap
+            while (less(a[++i], v)) {
+                if (i == hi) break;
+            }
+
+            // find item on hi to swap
+            while (less(v, a[--j])) {
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            }
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+    /**
+     * Rearranges the array so that {@code a[k]} contains the kth smallest key;
+     * {@code a[0]} through {@code a[k-1]} are less than (or equal to) {@code a[k]}; and
+     * {@code a[k+1]} through {@code a[n-1]} are greater than (or equal to) {@code a[k]}.
+     *
+     * @param  a the array
+     * @param  k the rank of the key
+     * @return the key of rank {@code k}
+     * @throws IllegalArgumentException unless {@code 0 <= k < a.length}
+     */
+    public static Comparable select(Comparable[] a, int k) {
+        if (k < 0 || k >= a.length) {
+            throw new IllegalArgumentException("index is not between 0 and " + a.length + ": " + k);
+        }
+        shuffle(a);
+        int lo = 0, hi = a.length - 1;
+        while (hi > lo) {
+            int i = partition(a, lo, hi);
+            if      (i > k) hi = i - 1;
+            else if (i < k) lo = i + 1;
+            else return a[i];
+        }
+        return a[lo];
+    }
+
+
+
+   /***************************************************************************
+    *  Helper sorting functions.
+    ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        if (v == w) return false;   // optimization when reference equals
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+    
+     public static void shuffle(Object[] ar) {
+
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+          int index = rnd.nextInt(i + 1);
+          // Simple swap
+          Object a = ar[index];
+          ar[index] = ar[i];
+          ar[i] = a;
+        }
+    }
+
+            
 }
+  
+
