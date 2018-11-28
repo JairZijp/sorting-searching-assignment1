ac04ef4d6b27e4fdabf40b39c5f398e93cb29d3e
JairZijp <jairzijp123@gmail.com>
2018-11-28 23:42:02 +0100

    quicksort now properly working

--- src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
+++ src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
@@ -2,28 +2,25 @@ package nl.hva.ict.ss;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Put your tests that show you implemented the code correctly in this class.
  * Any test placed at a different class will be ignored!
  * Failing to add tests here will result in failing the assignment!
  */
 public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
-    
-    
     @Test
-    public void checkIfSorted() {
-   
-        Player[] players = null;
-        Player[] players2 = null;
-        for (int numberOfPlayers = 1; numberOfPlayers < 100; numberOfPlayers++) {
-            players = getSubList(unsortedPlayers, numberOfPlayers).toArray(new Player[numberOfPlayers]);
-             
-        }
-        players2 = AdvancedSorts.quickSort(players);   
-        for (int i = 0; i < players2.length; i++) {
-            System.out.print("\nPlayer " + i + ": "+ players2[i].getHighScore());
-        }
-   
-    }
+    public void testArraySorted() {
+        Player jack = new Player("Jack", "Jones", 5000);
+        Player james = new Player("James", "Bond", 3000);
+        Player john = new Player("John ", "Doe", 7000);
 
+        Player[] players = new Player[]{jack,james,john};
+        Player[] sorted = AdvancedSorts.quickSort(players);
+
+        assertEquals(sorted[0], james);
+        assertEquals(sorted[1], jack);
+        assertEquals(sorted[2], john);
+    }
 }
